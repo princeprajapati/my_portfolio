a7512de8e5dcdbdf7aa0e9182404db2e26ad38c3
Prince Prajapati <pprajapati2316@conestogac.on.ca>
2024-04-27 16:07:20 -0400

    deployment

---
+++ build/precache-manifest.13c81abffabe5133e402061c73e70688.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92027ace126afc5c38c780b5d26f23e",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "3c8d2257a17868bee70f",
+    "url": "/my_portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "290e67a10584d47a53e5",
+    "url": "/my_portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "3c8d2257a17868bee70f",
+    "url": "/my_portfolio/static/js/2.2423a3f8.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/my_portfolio/static/js/2.2423a3f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "290e67a10584d47a53e5",
+    "url": "/my_portfolio/static/js/main.06842c9b.chunk.js"
+  },
+  {
+    "revision": "1107bcf17560ca4fe97c",
+    "url": "/my_portfolio/static/js/runtime-main.b6edbb39.js"
+  }
+]);
